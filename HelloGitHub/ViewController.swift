07626f379123a1d00fb55dc0f2de0ee193aa0ee7
若月　聖 <wakatsukiakira@wakatsukiseinoMacBook-Pro.local>
2019-10-19 16:20:24 +0900

    first commit

---
+++ HelloGitHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloGitHub
+//
+//  Created by 若月　聖 on 2019/10/19.
+//  Copyright © 2019 若月　聖. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
